f6d2b773b919e2d264486953d5366a7200180309
TangThiHoa <hoarua592001@gmail.com>
2022-10-19 15:26:07 +0700

    level 1

---
+++ src/main/java/com/example/th_sql/ThSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.th_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/th_sql/ThSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
